4d6eff37d5f6f1437ef1f9c0d47b09c9044183a0
ChenLin Wang <wangchenlin2001@icloud.com>
2024-11-14 13:26:48 +0800

    0.0.1 - Transfered from CpE Lab

--- out/script.js
+++ out/script.js
@@ -1,106 +1,253 @@
-var q = x=>document.querySelector(x);
-var Qf = x=>(f=>document.querySelectorAll(x).forEach(f));
-var Q = x=>Array.from(document.querySelectorAll(x));
-var tab = {
-    len: 0,
-    on: -1,
+// @ts-check
+
+/** @type {{send:(...x:any[])=>void,on:(...x:any[])=>void}} */
+// @ts-ignore
+const electron = window.electron ?? {
+    send: (...x) => console.log('ELECTRON SEND: ', x),
+    on: (...x) => console.log('ELECTRON ON: ', x),
 };
-if (electron == undefined) {
-    var electron = {
-        send: (...x) => console.log('ELECTRON SEND', x),
-        on: (...x) => console.log('ELECTRON ON', x)
-    };
+/** Browser API
+ * - Connection to `electron`
+ *   - `tab-open(id:number,url:string)` - Opens a tab
+ *   - `tab-go(step:number)` - Goes to a step in the tab focused history
+ *   - `tab-close` - Closes current tab focused
+ *   - `tab-show` - Shows browser
+ *   - `tab-hide` - Hides browser
+ * - Properties
+ *   - `dom` - Browser div element
+ *   - `tabs` - All tabs in browser
+ *   - `focus` - Current tab being shown
+ *   - `on_focus` - Triggered when a tab is focused/shown
+ *   - `on_blue` - Triggered when the browser is hidden
+ *   - `open` - Opens a tab
+ *   - `get_id` - Gets available id for new tab
+ *   - `go(n)` - Step in focused tab's history
+ *   - `close()` - Closes focused tab
+ *   - `show()` - Shows/Resizes browser
+ *   - `error()` - Shows browser error
+ */
+class Browser {
+    /* ----- PROPERTIES ----- */
+    /** @type {HTMLElement} */
+    dom;
+    /** @type {{[index:number]:{url:string}}} */
+    tabs = {};
+    /** @type {number} */
+    focus = -1;
+
+    /* ----- EVENT LISTENERS ----- */
+    /**
+     * Triggered when a tab becomes visible
+     * @param {number} id 
+     */
+    on_focus = id => {};
+    /**
+     * Triggered when no tabs are visible
+     */
+    on_blur = () => {};
+    /**
+     * Triggered when a tab changes url
+     * @param {number} id 
+     * @param {string} url 
+     */
+    on_delta_url = (id, url) => {};
+    /**
+     * Triggered when a tab changes title
+     * @param {number} id 
+     * @param {string} title 
+     */
+    on_delta_title = (id, title) => {};
+    /**
+     * Triggered when a tab changes favicon
+     * @param {number} id 
+     * @param {string} favicon 
+     */
+    on_delta_favicon = (id, favicon) => {};
+
+    /* ----- ACTIONS ----- */
+    /**
+     * Opens a tab
+     * - If `id` is null then creates a new tab
+     * - If `url` is not null then sets the tab url
+     * @param {number} id - ID of tab
+     * @param {string|null} url - URL of tab
+     */
+    open(id, url=null) {
+        this.dom.setAttribute('class','loading');
+        electron.send('tab-open', id, url);
+    }
+    /** @returns {number} Available ID for new tab */
+    new_id() {
+        let id = 0;
+        while (this.tabs[id] != undefined) id++;
+        return id;
+    }
+    /**
+     * Goes to a history path relative present
+     * @param {number} n - Step size
+     */
+    go(n) {
+        if (this.focus == -1) return this.error();
+        electron.send('tab-go', this.focus, n);
+    }
+    /**
+     * Closes the current tab in focus
+     */
+    close() {
+        if (this.focus == -1) return this.error();
+        electron.send('tab-close', this.focus);
+    }
+    /**
+     * Shows/Resizes the browser tab
+     */
+    show() {
+        let r = this.dom.getBoundingClientRect();
+        electron.send('tab-resize', r.left+2, r.top+2, r.width-4, r.height-4);
+    }
+    /**
+     * Shows error in tab
+     */
+    error() {
+        this.dom.setAttribute('class','error');
+        electron.send('tab-hide');
+    }
+    
+    /* ----- CONSTRUCTOR ----- */
+    /** @param {HTMLElement} dom */
+    constructor(dom) {
+        this.dom = dom;
+        window.addEventListener('resize', this.show);
+        
+        electron.on('url-changed', (id,url) => {
+            this.on_delta_url(id,url);
+        });
+        electron.on('title-changed', (id,title) => {
+            this.on_delta_title(id,title);
+        });
+        electron.on('favicon-changed', (id,url) => {
+            this.on_delta_favicon(id, url);
+        });
+        /*electron.on('browser-options', (...a) => {
+            let c = [true,a[0],tab.len!=0,a[1],tab.len!=0];
+            Qf('.side.browser>div.options>button')((b,n) => {
+                if (c[n]) b.removeAttribute('disabled');
+                else b.setAttribute('disabled','');
+            });
+        });*/
+    }
 }
 
-function tab_open() {
-    q('#browser').setAttribute('class','loading');
-    let index = Number(this.getAttribute('data-index'));
-    if (isNaN(index)) return;
-    tab.on = index;
-    Qf('#side>div:not(.options)')((d,n) => {
-        if (n == tab.on) d.classList.add('on');
-        else d.classList.remove('on')
-    });
-    electron.send('tab-open', index);
-}
-function tab_url() {
-    q('#browser').setAttribute('class','loading');
-    let index = Number(this.parentElement.getAttribute('data-index'));
-    if (isNaN(index)) return;
-    electron.send('tab-url', this.value);
-}
-function tab_new() {
-    q('#browser').setAttribute('class','loading');
-    tab.on = tab.len;
-    tab.len++;
-    let d = document.createElement('div');
-    d.innerHTML='<img onload="this.classList.add(\'loaded\')"></img><text></text><input placeholder="Page URL:"></input>';
-    d.querySelector('input').addEventListener('change', tab_url);
-    d.setAttribute('data-index', tab.on);
-    d.addEventListener('click', tab_open);
-    q('#side').insertBefore(d, q('#side>button'));
-    Qf('#side>div:not(.options)')((d,n) => {
-        if (n == tab.on) d.classList.add('on');
-        else d.classList.remove('on')
-    });
-    electron.send('tab-new', 'https://www.google.com');
-}
-function tab_close() {
-    q('#browser').setAttribute('class',tab.len > 1 ? 'loading' : '');
-    q('#side').removeChild(Q('#side>div:not(.options)')[tab.on]);
-    Qf('#side>div:not(.options)')((d,n) => d.setAttribute('data-index', n));
-    tab.on--;
-    tab.len--;
-    if (tab.on < 0 && tab.len) tab.on = 0;
-    Qf('#side>div:not(.options)')((d,n) => {
-        if (n == tab.on) d.classList.add('on');
-        else d.classList.remove('on')
-    });
-    electron.send('tab-close');
-}
+/* ----- USEFUL FUNCTIONS ----- */
+/**
+ * Gets a single query match
+ * @param {string} x - CSS Query
+ * @returns {HTMLElement|null}
+ */
+var q = x=>document.querySelector(x);
+/**
+ * Does a for each loop to each query
+ * @param {string} x - CSS Query
+ * @returns {(f:(x:HTMLElement,n:number)=>void)=>void}
+ */
+var Qf = x=>(f=>document.querySelectorAll(x).forEach((x,n)=>{
+    if (x instanceof HTMLElement) f(x,n);
+}));
+/**
+ * Returns an array of elements
+ * @param {string} x - CSS Query
+ * @returns {HTMLElement[]}
+ */
+var Q = x=>Array.from(document.querySelectorAll(x));
 
-q('#nav').addEventListener('click', function(e){
+var browser = new Browser(q('#browser')??document.createElement('div'));
+
+
+/** ----- BINDING ----- */
+/**
+ * Returns the tab element based on id and a function to deal with it
+ * @param {number} id 
+ * @param {(x:HTMLElement)=>void} f
+ */
+var QTab = (id,f) => Qf(`.side.browser>div:not(.options)[data-id=${id}]`)(f);
+browser.on_blur = () => Qf('.side.browser>div:not(.options).on')(d=>d.classList.remove('on'));
+browser.on_focus = id => {
+    browser.on_blur();
+    QTab(id,d=>d.classList.add('on'));
+};
+browser.on_delta_url = (id, url) => {
+    QTab(id,d=>{
+        let i = d.querySelector('input');
+        if (i) i.value = url;
+    });
+};
+browser.on_delta_title = (id, title) => {
+    QTab(id,d=>{
+        let i = d.querySelector('text');
+        // @ts-ignore
+        if (i) i.innerText = title;
+    });
+};
+browser.on_delta_favicon = (id, url) => {
+    QTab(id,d=>{
+        let i = d.querySelector('img');
+        if (i) i.src = url;
+    });
+};
+Qf('.side.browser>div.options>button')((d,n) => {
+    switch (n) {
+        case 0:
+            d.addEventListener('click', function(){
+                let id = browser.new_id();
+                let d = document.createElement('div');
+                d.setAttribute('data-id', String(id));
+                d.innerHTML='<img onload="this.classList.add(\'loaded\')"></img><text></text><input placeholder="Page URL:" value="www.google.com"></input>';
+                d.querySelector('input')?.addEventListener('change', () => {
+                    browser.open(id, d.querySelector('input')?.value??null);
+                });
+                d.addEventListener('click', ()=>browser.open(id));
+                q('.side.browser')?.appendChild(d);
+                d.click();
+            });
+            break;
+        case 1:
+            d.addEventListener('click', function(){
+                browser.go(-1);
+            });
+            break;
+        case 2:
+            d.addEventListener('click', function(){
+                browser.go(0);
+            });
+            break;
+        case 3:
+            d.addEventListener('click', function(){
+                browser.go(1);
+            });
+            break;
+        case 4:
+            d.addEventListener('click', function(){
+                browser.close();
+            });
+            break;
+    } 
+});
+
+
+q('#nav')?.addEventListener('click', function(e){
     let w = document.createElement('div');
     let r = this.getBoundingClientRect();
-    let p = q('#nav .ripples')
+    let p = q('#nav .ripples');
     w.classList.add('ripple');
     w.style.left = `${e.clientX-r.left}px`;
     w.style.top = `${e.clientY-r.top}px`;
-    p.appendChild(w);
-    setTimeout(()=>p.removeChild(w), 500);
+    p?.appendChild(w);
+    setTimeout(()=>p?.removeChild(w), 500);
 });
 Qf('#nav>button')(d=>d.addEventListener('click', function() {
-    console.log(this);
-    let t = this.querySelectorAll('text:last-child');
+    /** @type {HTMLElement[]} */
+    let t = Array.from(this.querySelectorAll('text:last-child'));
     if (t.length == 0) return;
     let n = t[0].innerText.toLowerCase();
     document.body.setAttribute('class', n);
+    if (n == 'browser') browser.show();
 }));
-
-q('.side.browser>div.options>button:nth-child(1)').addEventListener('click', tab_new);
-q('.side.browser>div.options>button:nth-child(2)').addEventListener('click', ()=>electron.send('tab-back'));
-q('.side.browser>div.options>button:nth-child(3)').addEventListener('click', ()=>electron.send('tab-reload'));
-q('.side.browser>div.options>button:nth-child(4)').addEventListener('click', ()=>electron.send('tab-next'));
-q('.side.browser>div.options>button:nth-child(5)').addEventListener('click', tab_close);
-
-window.addEventListener('resize', e => {
-    let r = q('#browser').getBoundingClientRect();
-    electron.send('tab-resize', r.left+2, r.top+2, r.width-4, r.height-4);
-});
-electron.on('url-changed', url => {
-    Q('#side>div:not(.options)')[tab.on].querySelector('input').value = url;
-});
-electron.on('title-changed', title => {
-    Q('#side>div:not(.options)')[tab.on].querySelector('text').innerText = title;
-});
-electron.on('favicon-changed', url => {
-    Q('#side>div:not(.options)')[tab.on].querySelector('img').src = url;
-});
-electron.on('browser-options', (...a) => {
-    let c = [true,a[0],tab.len!=0,a[1],tab.len!=0];
-    Qf('#side>div.options>button')((b,n) => {
-        if (c[n]) b.removeAttribute('disabled');
-        else b.setAttribute('disabled','');
-    });
-});
-window.dispatchEvent(new Event('resize'));
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import * as path from 'path';
  * Main browser window, contains `index.html`
  */
 var window:BrowserWindow|null = null;
-var tabs:WebContentsView[] = [];
+var tabs:{[id:number]:{view:WebContentsView}} = {};
 var tabon:number = -1;
 var size:number[] = [800, 800];
 var view_size:number[] = [0, 0, size[0], size[1]];
@@ -31,79 +31,66 @@ function load_window() {
  * Loads web tab
  * @param url {string} Url of page
  */
-function load_tab(url:string):void {
+function open_tab(id:number, url:string|null):void {
     if (window == null) return console.log('Window is not initialized');
-    if (tabon != -1) window.contentView.removeChildView(tabs[tabon]);
-    const view = new WebContentsView({
-        webPreferences: {
-            transparent: true,
-            nodeIntegration: true,
-        },
-    });
-    window.contentView.addChildView(view);
-    view.setBounds({x: view_size[0], y: view_size[1], width: view_size[2], height: view_size[3]});
-    view.webContents.loadURL(url);
-    view.webContents.on('did-navigate', (event, url) => {
-        console.log('BrowserView URL changed to:', url);
-        console.log(view.webContents.navigationHistory.canGoBack(), view.webContents.navigationHistory.canGoForward());
-        window?.webContents.send('url-changed', url);
-        window?.webContents.send('browser-options', view.webContents.navigationHistory.canGoBack(), view.webContents.navigationHistory.canGoForward());
-    });
-    view.webContents.on('page-title-updated', (event, title) => {
-        window?.webContents.send('title-changed', title);
-    });
-    view.webContents.on('page-favicon-updated', (event, favicons) => {
-        window?.webContents.send('favicon-changed', favicons);
-    });
-    tabon = tabs.length;
-    tabs.push(view);
+    if (tabs[id] == undefined) {
+        const view = new WebContentsView({
+            webPreferences: {
+                transparent: true,
+                nodeIntegration: true,
+            },
+        });
+        window.contentView.addChildView(view);
+        view.setBounds({x: view_size[0], y: view_size[1], width: view_size[2], height: view_size[3]});
+        view.webContents.loadURL(url ?? 'https://www.google.com');
+        view.webContents.on('did-navigate', (event, url) => {
+            //console.log('BrowserView URL changed to:', url);
+            //console.log(view.webContents.navigationHistory.canGoBack(), view.webContents.navigationHistory.canGoForward());
+            window?.webContents.send('url-changed', id, url);
+            //window?.webContents.send('browser-options', view.webContents.navigationHistory.canGoBack(), view.webContents.navigationHistory.canGoForward());
+        });
+        view.webContents.on('page-title-updated', (event, title) => {
+            window?.webContents.send('title-changed', id, title);
+        });
+        view.webContents.on('page-favicon-updated', (event, url) => {
+            window?.webContents.send('favicon-changed', id, url);
+        });
+        tabs[id] = {view:view};
+    } else {
+        if (tabon != -1) window.contentView.removeChildView(tabs[tabon].view);
+        if (url != null) tabs[id].view.webContents.loadURL(url);
+        window.contentView.addChildView(tabs[id].view);
+        window.webContents.send('tab-focus')
+    }
+    tabon = id;
+    tabs[id].view.setBounds({x: view_size[0], y: view_size[1], width: view_size[2], height: view_size[3]});
 }
 
 /* ----- CONNECTIONS ----- */
-ipcMain.on('tab-new', (event:IpcMainEvent, n:string) => {
-    load_tab(n);
+ipcMain.on('tab-open', (event:IpcMainEvent, id:number, url:string|null) => {
+    open_tab(id, url);
 });
 ipcMain.on('tab-resize', (event:IpcMainEvent, x:number, y:number, w:number, h:number) => {
     view_size = [x, y, w, h];
     if (tabon == -1) return;
-    tabs[tabon].setBounds({x: x, y: y, width: w, height: h});
+    tabs[tabon].view.setBounds({x: x, y: y, width: w, height: h});
 });
-ipcMain.on('tab-open', (event:IpcMainEvent, n:number) => {
-    if (n < 0 || tabon == n || n >= tabs.length) return;
-    if (tabon != -1) window?.contentView.removeChildView(tabs[tabon]);
-    window?.contentView.addChildView(tabs[n]);
-    tabs[n].setBounds({x: view_size[0], y: view_size[1], width: view_size[2], height: view_size[3]});
-    tabon = n;
-    window?.webContents.send('browser-options', false, false);
+ipcMain.on('tab-go', (event:IpcMainEvent, id:number, step:number) => {
+    if (step < 0) {
+        for (let n = 0; n < -step; n++) tabs[id].view.webContents.navigationHistory.goBack();
+    } else if (step > 0) {
+        for (let n = 0; n < step; n++) tabs[id].view.webContents.navigationHistory.goForward();
+    } else tabs[id].view.webContents.reload();
 });
-ipcMain.on('tab-url', (event:IpcMainEvent, u:string) => {
-    if (tabon == -1) return;
-    tabs[tabon].webContents.loadURL(u);
-});
-ipcMain.on('tab-back', (event:IpcMainEvent) => {
-    if (tabon == -1) return;
-    tabs[tabon].webContents.navigationHistory.goBack();
-    window?.webContents.send('browser-options', tabs[tabon].webContents.navigationHistory.canGoBack(), true);
-});
-ipcMain.on('tab-next', (event:IpcMainEvent) => {
-    if (tabon == -1) return;
-    tabs[tabon].webContents.navigationHistory.goForward();
-    window?.webContents.send('browser-options', true, tabs[tabon].webContents.navigationHistory.canGoForward());
-});
-ipcMain.on('tab-reload', (event:IpcMainEvent) => {
-    if (tabon == -1) return;
-    tabs[tabon].webContents.reload();
-});
-ipcMain.on('tab-close', (event:IpcMainEvent) => {
-    if (tabon == -1) return;
-    window?.contentView.removeChildView(tabs[tabon]);
-    tabs.splice(tabon, 1);
-    tabon--;
-    if (tabon == -1 && tabs.length) tabon = 0;
+ipcMain.on('tab-close', (event:IpcMainEvent, id:number) => {
+    window?.contentView.removeChildView(tabs[id].view);
+    delete tabs[id];
+    
+    /*if (tabon == -1 && tabs.length) tabon = 0;
     if (tabon != -1) {
         window?.contentView.addChildView(tabs[tabon]);
         window?.webContents.send('browser-options', tabs[tabon].webContents.navigationHistory.canGoBack(), tabs[tabon].webContents.navigationHistory.canGoForward());
-    } else window?.webContents.send('browser-options', false, false);
+    } else window?.webContents.send('browser-options', false, false);*/
 });
 
 /* ----- BINDING ----- */
